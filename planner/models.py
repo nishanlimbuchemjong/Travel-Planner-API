520a14fe059847dff807179072d945ebcaa43d51
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2025-03-12 14:08:14 +0545

    model, views, urls, and serializers added for travel planner api

---
+++ planner/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+class Destination(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    image = models.URLField()  # URL of the destination image
+    description = models.TextField()
+    local_experiences = models.TextField()
+    estimated_budget = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+class Attraction(models.Model):
+    destination = models.ForeignKey(Destination, on_delete=models.CASCADE, related_name="top_attractions")
+    name = models.CharField(max_length=100)
+    image = models.URLField()  # URL of the attraction image
+    location = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Itinerary(models.Model):
+    trip_name = models.CharField(max_length=100)
+    destinations = models.ManyToManyField(Destination)  # Many destinations in one itinerary
+    total_trip_duration = models.IntegerField()  # Duration in days
+    estimated_budget = models.DecimalField(max_digits=10, decimal_places=2)
+    daily_itinerary = models.JSONField()  # Store daily itinerary as JSON
+
+    def __str__(self):
+        return self.trip_name
---
+++ planner/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import Destination, Attraction, Itinerary
+
+class AttractionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attraction
+        fields = ['id', 'name', 'image', 'location']
+
+class DestinationSerializer(serializers.ModelSerializer):
+    top_attractions = AttractionSerializer(many=True, required=False) 
+
+    class Meta:
+        model = Destination
+        fields = ['id', 'name', 'country', 'image', 'description', 'local_experiences', 'estimated_budget', 'top_attractions']
+
+class ItinerarySerializer(serializers.ModelSerializer):
+    destinations = serializers.PrimaryKeyRelatedField(queryset=Destination.objects.all(), many=True)
+
+    class Meta:
+        model = Itinerary
+        fields = ['id', 'trip_name', 'destinations', 'total_trip_duration', 'estimated_budget', 'daily_itinerary']
