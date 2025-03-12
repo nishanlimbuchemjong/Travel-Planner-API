f8156f8d4292bc3b04e7f780a9cce0c223ea568b
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2025-03-12 15:07:56 +0545

    added render link of my api on setting.py file

--- travel_planner_api/settings.py
+++ travel_planner_api/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'your-app-name.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://travel-planner-api-qgwf.onrender.com']
 
 
 # Application definition
