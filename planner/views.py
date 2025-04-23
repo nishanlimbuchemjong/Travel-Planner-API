b6ed87fe9958355891e21d97a717993c950f61a9
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2025-04-23 06:56:00 +0545

    added BlogListCreate and BlogDetail views on views.py file

--- planner/views.py
+++ planner/views.py
@@ -1,8 +1,8 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Destination, Itinerary, Attraction
-from .serializers import DestinationSerializer, ItinerarySerializer, AttractionSerializer
+from .models import Destination, Itinerary, Attraction, Blog
+from .serializers import DestinationSerializer, ItinerarySerializer, AttractionSerializer, BlogSerializer
 from django.http import JsonResponse
 
 # Destination API views
@@ -29,3 +29,18 @@ class AttractionList(APIView):
             return JsonResponse(serializer.data, safe=False)  # Return the attractions in JSON format
         except Destination.DoesNotExist:
             return JsonResponse({"error": "Destination not found"}, status=404)
+        
+class BlogListCreate(APIView):
+    def get(self, request):
+        blogs = Blog.objects.all()
+        serializer = BlogSerializer(blogs, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+class BlogDetail(APIView):
+    def get(self, request, blog_id):
+        try:
+            blog = Blog.objects.get(id=blog_id)
+            serializer = BlogSerializer(blog)
+            return JsonResponse(serializer.data, safe=False)
+        except Blog.DoesNotExist:
+            return JsonResponse({'error': 'Blog not found'}, status=404)
