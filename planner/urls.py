64844bd0758fe0e2dc61748ea0963f72fa1f6124
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2025-04-23 06:56:31 +0545

    added urls for blog and blog details on urls.py file

--- planner/urls.py
+++ planner/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('destinations/', views.DestinationListCreate.as_view(), name='destination-list-create'),
     path('itineraries/', views.ItineraryListCreate.as_view(), name='itinerary-list-create'),
-    path('destinations/<int:destination_id>/attractions/', views.AttractionList.as_view(), name='attraction-list')
+    path('destinations/<int:destination_id>/attractions/', views.AttractionList.as_view(), name='attraction-list'),
+    path('blogs/', views.BlogListCreate.as_view(), name='blog-list'),
+    path('blogs/<int:blog_id>/', views.BlogDetail.as_view(), name='blog-detail'),
 ]
